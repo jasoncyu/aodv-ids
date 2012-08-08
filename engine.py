540a531faa874b88625be1e7015e6700d41fe2dd
Jason Yu <yujason2@gmail.com>
2012-08-08 02:27:24 -0500

    Wrote engine.py to run multiple simulations

---
+++ engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import subprocess
+
+# print command
+# subprocess.call(["ls", "-l"])
+# w_values = [0.2, 0.4, 0.6, 0.8, 1.0]
+w_values = [0.2, 0.4]
+for w in w_values:
+    # print 'hi'
+    command = './waf "--run=scratch/aodv/aodv --w=%f"' % w
+    subprocess.call(command, shell=True)
+# subprocess.call("exit 1", shell=True)
+
