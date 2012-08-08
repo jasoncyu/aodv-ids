85a990501694dc3b6280426645bcbb34a29ae73c
Jason Yu <yujason2@gmail.com>
2012-08-08 01:06:42 -0500

    Refactored aodv into header and source.

--- scratch/aodv/aodv.cc
+++ scratch/aodv/aodv.cc
@@ -19,29 +19,8 @@
 
  //TODO: mess with random seed
 
-#include "ns3/aodv-module.h"
-#include "ns3/core-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/wifi-module.h"
-#include "ns3/v4ping-helper.h"
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <functional>
-#include <numeric>
-#include <cassert>
-#include <iomanip>
-//I made these
-#include "common.h"
-#include "cluster.h"
-#include "cluster_algorithm.h"
-#include "table.h"
+#include "aodv.h"
 
-using namespace ns3;
 using namespace std;
 
 void ReceivePacket (Ptr<Socket> socket)
@@ -62,106 +41,6 @@ static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
     {
       socket->Close ();
     }
-}
-
-
-// static void WhatTimeIsIt() {
-//   std::cout << "It is now " << Simulator::Now().GetSeconds () << " seconds " << std::endl; 
-// }  
-
-
-/**
- * \brief Test script.
- * 
- * This script creates 1-dimensional grid topology and then ping last node from the first one: *
- * [10.0.0.1] <-- step --> [10.0.0.2] <-- step --> [10.0.0.3] <-- step --> [10.0.04]
- * 
- * ping 10.0.0.4
- */
-class AodvExample 
-{
-public:
-  AodvExample ();
-  /// Configure script parameters, \return true on successful configuration
-  bool Configure (int argc, char **argv);
-  /// Run simulation
-  void Run ();
-
-private:
-  ///\name parameters
-  //\{
-  /// Number of nodes
-  uint32_t size;
-  /// Distance between nodes, meters
-  double step;
-  /// Simulation time, seconds
-  double totalTime;
-  /// Write per-device PCAP traces if true
-  bool pcap;
-  /// Print routes if true
-  bool printRoutes;
-  double rss;  // -dBm
-  uint32_t packetSize; // bytes
-  uint32_t numPackets;
-  double interval; // seconds
-  bool verbose;
-  bool malicious;
-  bool trace;
-
-  //simulation parameters
-  double threshold;
-
-  double w;
-
-  //The only sample in this file, the sample
-  //from our ONE monitor node
-  Sample sample;
-  Clusters labelled_clusters;
-  //\}
-
-  ///\name network
-  //\{
-  NodeContainer nodes;
-  NetDeviceContainer devices;
-  Ipv4InterfaceContainer interfaces;
-  //\}
-
-private:
-  void w_cluster_table(vector<double> x, vector<int> y, vector<int> z);
-  /// Uses training_data to generate training_clusters
-  void Training();
-  // Attack scenario tested.
-  void Testing();
-  void Log(std::ostringstream& os, std::string name = "AODV_LOG");
-  void AggregateTraffic(Ptr<Node> node);
-  void TrainingDataTable(Sample s);
-
-  void CreateNodes ();
-  void CreateDevices ();
-  //Set monitor and malicious nodes
-  //Set callback needed to get data from monitors  
-  void InstallInternetStack ();
-  //install traffic apps. 
-  void InstallApplications ();
-};
-
-int main (int argc, char **argv)
-{
-  AodvExample test;
-  if (!test.Configure (argc, argv))
-    NS_FATAL_ERROR ("Configuration failed. Aborted.");
-
-  test.Run ();
-
-  // Cluster c; // vector<double> sample; // sample.push_back(0.0);
-  // sample.push_back(1.0);
-
-  // pair<int, vector<double> > entry;
-  // entry.first = 0;
-  // entry.second = sample;
-
-  // c.add(entry);
-  return 0;
 }
 
 //-----------------------------------------------------------------------------
@@ -355,6 +234,13 @@ AodvExample::Training() {
   oss << "Number of anom clusters: " << numberAnomClusters << endl;
   // w_cluster_table(w_values, numCluster, numAnom);
   Log(oss);
+}
+
+void
+AodvExample::Testing() 
+{
+
+
 }
 
 //plots w|clusters_total|clusters_anon
@@ -560,4 +446,23 @@ AodvExample::AggregateTraffic(Ptr<Node> node) {
   routing->GetMonitoredData(traffic); 
 
   sample.push_back(traffic);
+}
+
+int main (int argc, char **argv)
+{
+  AodvExample test;
+  if (!test.Configure (argc, argv))
+    NS_FATAL_ERROR ("Configuration failed. Aborted.");
+
+  test.Run ();
+
+  // Cluster c; // vector<double> sample; // sample.push_back(0.0);
+  // sample.push_back(1.0);
+
+  // pair<int, vector<double> > entry;
+  // entry.first = 0;
+  // entry.second = sample;
+
+  // c.add(entry);
+  return 0;
 }
---
+++ scratch/aodv/aodv.h
@@ -0,0 +1,89 @@
+#include "ns3/aodv-module.h"
+#include "ns3/core-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/v4ping-helper.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <functional>
+#include <numeric>
+#include <cassert>
+#include <iomanip>
+//I made these
+#include "common.h"
+#include "cluster.h"
+#include "cluster_algorithm.h"
+#include "table.h"
+
+using namespace ns3;
+class AodvExample 
+{
+public:
+  AodvExample ();
+  /// Configure script parameters, \return true on successful configuration
+  bool Configure (int argc, char **argv);
+  /// Run simulation
+  void Run ();
+
+private:
+  ///\name parameters
+  //\{
+  /// Number of nodes
+  uint32_t size;
+  /// Distance between nodes, meters
+  double step;
+  /// Simulation time, seconds
+  double totalTime;
+  /// Write per-device PCAP traces if true
+  bool pcap;
+  /// Print routes if true
+  bool printRoutes;
+  double rss;  // -dBm
+  uint32_t packetSize; // bytes
+  uint32_t numPackets;
+  double interval; // seconds
+  bool verbose;
+  bool malicious;
+  bool trace;
+
+  //simulation parameters
+  double threshold;
+
+  double w;
+
+  //The only sample in this file, the sample
+  //from our ONE monitor node
+  Sample sample;
+  Clusters labelled_clusters;
+  //\}
+
+  ///\name network
+  //\{
+  NodeContainer nodes;
+  NetDeviceContainer devices;
+  Ipv4InterfaceContainer interfaces;
+  //\}
+
+private:
+  void w_cluster_table(vector<double> x, vector<int> y, vector<int> z);
+  /// Uses training_data to generate training_clusters
+  void Training();
+  // Attack scenario tested.
+  void Testing();
+  void Log(std::ostringstream& os, std::string name = "AODV_LOG");
+  void AggregateTraffic(Ptr<Node> node);
+  void TrainingDataTable(Sample s);
+
+  void CreateNodes ();
+  void CreateDevices ();
+  //Set monitor and malicious nodes
+  //Set callback needed to get data from monitors  
+  void InstallInternetStack ();
+  //install traffic apps. 
+  void InstallApplications ();
+};
